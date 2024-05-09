ae7d2e90a3973f8ca1c6aa61dd23405907f1c56a
Fredy M. Jordan C <fmaxjordan@gmail.com>
2024-05-09 12:02:37 -0500

    agregar vista y plantillas 1

---
+++ .history/productos/views_20240509115035.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Producto
+
+# Create your views here.
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar_productos.html', {'productos': productos})
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        precio = request.POST.get('precio')
+        cantidad = request.POST.get('cantidad')
+        producto_nuevo = Producto(nombre=nombre, precio=precio, cantidad=cantidad)
+        producto_nuevo.save()
+        return redirect('rlistar_productos')
+    return render(request, 'agregar_productos.html')
